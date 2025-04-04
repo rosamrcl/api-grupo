1fb33ef9c5c7bcbae23ad53bd6d93e5158f9e2a3
BananaSpritee <biscoit01@hotmail.com>
2025-04-04 16:44:13 -0300

    Criação do arquivo Output.php e comentarios.
    O arquivo output.php serve como caminho para as funções assim deixando o código mais limpo e organizado.

--- api/index.php
+++ api/index.php
@@ -1,8 +1,16 @@
 <?php
 
+// Importar output.php ===================================================================== //
+
+require_once('output.php');
+
+// Resposta Padrão ========================================================================= //
+
 $data = [];
 $data['status'] = 'ERROR';
 $data['data'] = null;
+
+// Pedido API ============================================================================== //
 
 if(isset($_GET['option'])){
     switch($_GET['option']){
@@ -20,36 +28,8 @@ if(isset($_GET['option'])){
     }
 }
 
+// Resposta API ============================================================================= //
+
 response($data);
 
-function define_response(&$data, $value){
-    $data['status'] = ' SUCCESS';
-    $data['data'] = $value;
-}
-
-function data_time(&$data){
-    $data['status'] = 'DATA E HORA LOCAL SERVIDOR API';
-    date_default_timezone_set('America/Maceio');
-    $data['data'] = date("d-m-Y H:i:s");
-}
-
-function random_number(&$data, $num1, $num2){
-            if(isset($_GET['num1'])){
-                $num1 = intval($_GET['num1']);
-            }
-            if(isset($_GET['num2'])){
-                $num2 = intval($_GET['num2']);
-            }
-            if($num1 >= $num2){
-                response($data);
-                return;
-            }
-
-            define_response($data, rand($num1, $num2));
-}
-
-function response($data_response){
-    header("Content-Type:application/json");
-    echo json_encode($data_response);
-}
 ?>
