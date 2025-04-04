5f7136838468856f05dd5a52d3826c85f35b901e
ansilv0 <164552204+ansilv00@users.noreply.github.com>
2025-04-04 17:23:51 -0300

    .

--- app/app.php
+++ app/app.php
@@ -1,7 +1,29 @@
 <?php
 define ('API_BASE', 'http://localhost/api-grupo/api/index.php?option=' );
 
-echo'<h1>API GRUPO</h1>'
+    echo'<p>API GRUPO</p>';
 
+    for($i = 0; $i < 10; $i++)
+    {
+
+        $result = api_request('random&min=aaa&max=150');
+
+        if($result ['status'] == 'ERROR')
+        {
+            die('Aconteceu um erro na minha chamada da API');
+        }
+
+        echo "o valor aleatorio é: " . $result['name'] . "</br>";
+}
+
+    echo 'Finalizado';
+    
+function api_request($named)
+{
+        $client = curl_init(API_BASE . $named);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        return json_decode($response, true);
+}
 
 ?>
