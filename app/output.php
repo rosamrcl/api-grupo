b1ccaa447554cc89197a29542555986afbda8220
Rosa <costurezati@gmail.com>
2025-04-07 09:35:36 -0300

    .

--- app/output.php
+++ app/output.php
@@ -1,22 +1,17 @@
 <?php
-
 // Resposta Padrão ========================================================================= //
 
 function define_response(&$data, $value){
     $data['status'] = 'SUCCESS';
     $data['data'] = $value;
 }
-
 // Resposta Data =========================================================================== //
-
 function data_time(&$data){
     $data['status'] = 'DATA E HORA LOCAL SERVIDOR API';
     date_default_timezone_set('America/Maceio');
     $data['data'] = date("d-m-Y H:i:s");
 }
-
 // Resposta Número Aleatório ==============================================================  //
-
 function random_number(&$data, $num1, $num2){
             if(isset($_GET['num1'])){
                 $num1 = intval($_GET['num1']);
@@ -31,9 +26,7 @@ function random_number(&$data, $num1, $num2){
 
             define_response($data, rand($num1, $num2));
 }
-
 // Arquivo JSON ============================================================================ //
-
 function response($data_response){
     header("Content-Type:application/json");
     echo json_encode($data_response);
